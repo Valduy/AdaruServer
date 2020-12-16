43897bc7c33093ba6e74f6701da68f6e203c795b
Глеб Мельников <whnpir@gmail.ru>
2020-12-16 18:59:30 +0300

    Еще немного поменял структуру. Положил начало моделям представления.

---
+++ AdaruServer/ViewModels/TaskViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AdaruServer.Models;
+
+namespace AdaruServer.ViewModels
+{
+    public class TaskViewModel
+    {
+        public Models.Task Task { get; set; }
+        public ICollection<Tag> Tags { get; set; }
+    }
+}
